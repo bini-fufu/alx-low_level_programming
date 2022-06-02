881dbcf241598197a84b5f07300e9e96c25e2120
bini-fufu <se.biniyam.endale@gmail.com>
2022-06-02 15:43:30 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - printing
+ *
+ *Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" -Dora Korpar, 2015-10-19");
+
+	return (1);
+}
