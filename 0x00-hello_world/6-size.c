d8d3f1e949c92062a1662ec1311c57eab325c856
bini-fufu <se.biniyam.endale@gmail.com>
2022-06-02 15:29:43 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main -Prints character size
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int inttype;
+	char chartype;
+	long int longinttype;
+	long long int longlonginttype;
+	float floattype;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(chartype));
+	printf("Size of an int: %zu byte(s)\n", sizeof(inttype));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longinttype));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlonginttype));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floattype));
+
+	return (0);
+}
