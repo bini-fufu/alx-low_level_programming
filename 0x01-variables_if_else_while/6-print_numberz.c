debbbdd347f0b697fb480c0767567f9d1b90ddf2
bini-fufu <se.biniyam.endale@gmail.com>
2022-06-03 14:43:45 -0700

    char numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints 0-9
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
