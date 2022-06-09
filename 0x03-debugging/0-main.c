9f12f0b7ffa9c65798a610abba86bd3a7edc67b1
bini-fufu <se.biniyam.endale@gmail.com>
2022-06-09 03:09:26 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests function that prints if intger is pos or neg
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
