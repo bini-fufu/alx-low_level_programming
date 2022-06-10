3348e81ba3c590147a0bb0b84a33a1f2a30259a4
bini-fufu <se.biniyam.endale@gmail.com>
2022-06-10 14:11:32 -0700

    multiple

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - prints multiple of 2 numbers
+ * Description: multiply two number
+ * @a: accept integer value
+ * @b: accept intger value
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
