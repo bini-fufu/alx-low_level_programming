cfdb88a04eb341f2ed0f01777948b9ee7a44ed64
bini-fufu <se.biniyam.endale@gmail.com>
2022-06-24 22:53:28 +0000

    static

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - checks is a characte is uppercase
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
+ */
+int _islower(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
